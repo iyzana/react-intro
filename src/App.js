49133c1666f16c7741088a0982d3d10e693f0228
succcubbus <jannis.kaiser2@gmail.com>
2018-11-06 23:29:03 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hi name="you" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function Hi(props) {
+  return <h3>&lt;3 {props.name}</h3>;
+}
+
+export default App;
